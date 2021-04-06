940f13482300643a2e1a6e7e36b40340fff173c2
Jacobu <jacob.arlo.leibeck@gmail.com>
2021-04-05 20:19:14 -0400

    Pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ness Mains are awful people irl'));
+app.get('/', (req, res) => res.send('I love you Frankie!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
